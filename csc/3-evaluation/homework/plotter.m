b8bba755e678624f6dda01ba49f5cd33bf8d781c
MikhailErofeev <m-erofeev@yandex-team.ru>
2013-10-10 15:44:49 +0400

    play with tailor series

--- csc/3-evaluation/homework/plotter.m
+++ csc/3-evaluation/homework/plotter.m
@@ -1,64 +1,42 @@
 clear ; close all; clc
 
 data = load('learn.txt');
-x = data(:, 1); y = data(:, 2);
-m = length(y); % number of training examples
+xlearn = data(:, 1); ylearn = data(:, 2);
+m = length(ylearn); % number of training examples
 
-theta = zeros(size(x, 2), 1);
-[X mu sigma] = featureNormalize(x);
-X = [ones(m,1), X];
-
-theta = pinv(X' * X)*X'*y;
-val = X * theta;
-mse = sum((y - val).^2)/m;
-lastMse = mse;
-powers = [0, 1];
-size(X)
-for pw = -3:1:3
-	[candidat mu sigma] = featureNormalize(x.^pw);
-	X = [X, candidat];
-	theta = pinv(X' * X)*X'*y;
+theta = [];
+X = [];
+tailor_series_size = 20
+for n = 1:1:tailor_series_size
+	feature = (-1)^(n + 1) / n * (xlearn - 1).^n;	 
+	X = [X, feature];
+	theta = pinv(X' * X)*X'*ylearn;
 	val = X * theta;
-	mse = sum((y - val).^2)/m;
-	fprintf('add pow %f\tmean err %f\tdiv %f\tmse %f\n', pw,  mean(y - val), std(y-val), mse);
-	if lastMse <= mse
-		fprintf('less!\n');
-		X = resize(X, m, size(X,2));
-	else
-		powers = [powers, pw];
-	endif
-	lastMse = mse;
+	fprintf('mse %f\n', sum((ylearn - val).^2)/m);	
 endfor
 
-theta = pinv(X' * X)*X'*y;
-val = X * theta;	
-mse = sum((y - val).^2)/m
-size(X)
-powers
-
-
- plot(x, y, 'rx', 'MarkerSize', 3, 'color', 'green'); 
- figure
+X
+theta
 
 data = load('test.txt');
 x = data(:, 1); y = data(:, 2);
 m = length(x);
-[X mu sigma] = featureNormalize(x);
-size(X)
-for pw = powers
-	[feature mu sigma] = featureNormalize(x.^pw);
+
+X = [];
+for n = 1:1:tailor_series_size
+	feature = (-1)^(n + 1) / n * (x - 1).^n;	 
 	X = [X, feature];
 endfor
-size(X)
 val = X * theta;
 mse = sum((y - val).^2)/m
 
 
+ plot(xlearn, ylearn, 'rx', 'MarkerSize', 3, 'color', 'green'); 
+ figure
 plot(x, y, 'rx', 'MarkerSize', 3, 'color', 'blue'); 
+ figure; 
 plot(x, val, 'rx', 'MarkerSize', 3, 'color', 'red'); 
 %plot(x, val, 'rx', 'MarkerSize', 3); 
- xlabel('x'); 
- ylabel('ln(x)');
  figure; 
 
 
