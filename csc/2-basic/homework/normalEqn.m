8c6f748a412dc2a03e49ce3df4e200a37f0724b6
m-erofeev <m-erofeev@yandex-team.ru>
2013-10-17 16:11:17 +0400

    start factors analytic

--- csc/2-basic/homework/normalEqn.m
+++ csc/2-basic/homework/normalEqn.m
@@ -1,4 +1,4 @@
-function [theta] = normalEqn(X, y)
+function [theta, diff] = normalEqn(X, y)
 %NORMALEQN Computes the closed-form solution to linear regression 
 %   NORMALEQN(X,y) computes the closed-form solution to linear 
 %   regression using the normal equations.
@@ -6,5 +6,8 @@ function [theta] = normalEqn(X, y)
 theta = zeros(size(X, 2), 1);
 
 theta = pinv(X' * X)*X'*y;
+
+predict = transpose((transpose(theta) * transpose(X)));
+diff = (predict .- y) .^2;
 
 end
