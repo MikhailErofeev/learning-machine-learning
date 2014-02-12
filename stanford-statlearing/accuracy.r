54e6a857bfa593e90e61f0e32538352643856bd3
m-erofeev <m-erofeev@yandex-team.ru>
2014-02-12 13:52:56 +0400

    play with r, accurracy

--- stanford-statlearing/accuracy.r
+++ stanford-statlearing/accuracy.r
@@ -17,7 +17,7 @@ x <- vector(mode = "double", length = n)
 y <- vector(mode = "double", length = n)
 for (i in 1:n){
   x[i] = i
-  y[i] = realFunction(i) + 10 * rnorm(1)
+  y[i] = realFunction(i) + 0 * rnorm(1)
 }
 #plot(x,y)
 
@@ -27,16 +27,19 @@ sampleY <- vector(mode = "double", length = sampleSize)
 for (i in 1:sampleSize){
   r = sample(x, 1)
   sampleX[i] = r
-  sampleY[i] = realFunction(r) + 10 * rnorm(1)
+  sampleY[i] = realFunction(r) + 0 * rnorm(1)
 }
 plot(sampleX, sampleY)
 
 real = sapply(seq(1, sampleSize), realFunction)
-print(real)
+print(sampleX)
 print(sampleY)
-rss = rss(real, sampleY)
-curve(real, 0, n, xname = "t", add = TRUE)
+rss = rss(real, sampleY) #не верно, там треш, а не соотношение. нужно сделать регрессию по сэмплу и соотносить одинаковые x
+rse = sqrt(rss/(sampleSize - 2)) #residual standard error
 print(rss)
+print(rse)
+lo <- loess(y~x)
+lines(predict(lo), col='red', lwd=2)
 #var(10 * rnorm(n))/n
 #rands = rnorm(n)
 #mean((rands - mean(rands))*2)
